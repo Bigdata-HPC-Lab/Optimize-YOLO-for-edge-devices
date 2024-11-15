5fd3c49793b44b83f4e5d9638f254508234eb705
SeunghwanKim <pmy92769276@gmail.com>
2024-11-15 19:06:17 +0900

    Update video_multi.py

--- run/video_multi.py
+++ run/video_multi.py
@@ -6,62 +6,37 @@ import torch.multiprocessing as mp
 import os
 import glob
 
-proc_frames = 0
-numWhileLoop = 0
-video_read_time = 0
-model_load_time = 0
-
-YOLOv8StartTime = time.time()
 def process_video_read_multiprocessing(group_number):
-    print(group_number)
-    global numWhileLoop, proc_frames
-    global video_read_time
-    global model_load_time
-
     cap = cv2.VideoCapture(video_path)
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_jump_unit * group_number)
 
-    #Loop throgh the video frames
+    #Loop throgh the incomming data
     while cap.isOpened():
-        model_load_start_time = time.time()
-        print("KSH: START Model Load")
-        model = YOLO('yolov8n.pt')
-        model_load_end_time = time.time()
-        model_load_time = model_load_end_time - model_load_start_time
-        print("KSH: model_load_time: ", model_load_time)
-        print("KSH: END Model Load")
-
+        model = YOLO("Selcet YOLO model")
+        
         while proc_frames < frame_jump_unit:
-            print("KSH: while loop count: ", numWhileLoop)
-            numWhileLoop = numWhileLoop + 1
-            #Read a frame form the video
-            video_read_start_time = time.time()
+            #Read incomming data
             success, frame = cap.read()
-            video_read_end_time = time.time()
-            video_read_time += video_read_end_time - video_read_start_time
-            print("KSH: video_read_time: ", video_read_time)
+
             if success:
-                #Run YOLOv8 inference on the frame
+                #Run YOLO inference on the frame
                 results = model(frame, verbose=False)
                 proc_frames = proc_frames + 1
             else:
                 #break the loop of the end of the video is reached
                 cap.release()
                 break
-                #cap.release()
         else:
             cap.release()
 
-print("KSH: START video Open")
-#Open the video file
-video_path = "/mnt/data/elefant.mp4"
+#Open the incomming data file
+video_path = "Path to your data"
 cap = cv2.VideoCapture(video_path) 
-print("KSH: END video Open")
 
-#Number of parallel processes
+#Number of available cores
 num_processes = 1
 
-#Total frame counts of the video file
+#Total frame counts of the incomming data
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 #Calculate the frame_jump_unit
@@ -74,14 +49,4 @@ def multi_process():
     pool.close()
     pool.join()
 
-print("KSH: START Function multi_process")
 multi_process()
-#process_video_read_multiprocessing(1)
-
-print("KSH: END Function multi_process")
-
-YOLOv8EndTime = time.time()
-YOLOv8TotalTime = YOLOv8EndTime - YOLOv8StartTime
-print("KSH: YOLOv8TotalTime ", YOLOv8TotalTime )
-print("KSH: video_read_time ", video_read_time )
-
